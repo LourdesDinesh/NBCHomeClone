bb0da3b3478316cb437c9d23ba305ab3870149a7
LourdesDinesh <lourdesdinesh.s@tringapps.com>
2019-11-20 18:21:36 +0530

    Initial Project Commit

---
+++ NBCHomeClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NBCHomeClone
+//
+//  Created by Tringapps on 11/20/19.
+//  Copyright © 2019 Tringapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
