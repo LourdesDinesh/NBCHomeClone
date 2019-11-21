0c5c27f01f0a1efb866e8761ca2c0b027d48c64d
LourdesDinesh <lourdesdinesh.s@tringapps.com>
2019-11-21 14:04:52 +0530

    Completed Network Calling class

---
+++ NBCHomeClone/DataModel/Home.swift
@@ -0,0 +1,12 @@
+//
+//  Home.swift
+//  NBCHomeClone
+//
+//  Created by Tringapps on 11/21/19.
+//  Copyright © 2019 Tringapps. All rights reserved.
+//
+
+import Foundation
+struct Home:Decodable {
+    var msg:String!;
+}
--- NBCHomeClone/ViewController.swift
+++ NBCHomeClone/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let HOME_API:String = "https://demo3978672.mockable.io/";
+    let dispatchGroup:DispatchGroup = DispatchGroup();
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("HomeViewController loaded");
+        fetchData();
+        dispatchGroup.notify(queue: .main, execute: { [weak self] in
+            self?.loadTableView();
+        })
     }
-
-
+    
+    public func fetchData() {
+        dispatchGroup.enter();
+        FetchDataFromAPI.getJsonArrayFromApi(fromUrl: HOME_API) {[weak self] (home:Home) in
+            print(home.msg!);
+            self?.dispatchGroup.leave();
+        }
+    }
+    
+    public func loadTableView() {
+        
+    }
 }
 
