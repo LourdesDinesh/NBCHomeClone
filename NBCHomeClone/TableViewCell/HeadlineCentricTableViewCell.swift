2c16af633246441c76357dd0bbbb681281b7464c
LourdesDinesh <lourdesdinesh.s@tringapps.com>
2019-11-25 15:57:48 +0530

    Have completed the HeadlineCentricCell and have also implemented the classes for it

---
+++ NBCHomeClone/TableViewCell/HeadlineCentricTableViewCell.swift
@@ -0,0 +1,52 @@
+//
+//  HeadlineCentricTableViewCell.swift
+//  NBCHomeClone
+//
+//  Created by Tringapps on 11/25/19.
+//  Copyright © 2019 Tringapps. All rights reserved.
+//
+
+import UIKit
+
+class HeadlineCentricTableViewCell: UITableViewCell {
+    public static let REUSABLE_IDENTITY:String = "headlineCentricTableViewCell";
+    @IBOutlet weak var articleTypeImage: UIImageView!
+    @IBOutlet weak var articleTitle: UILabel!
+    @IBOutlet weak var updatedTime: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+
+    public func setValue(value: Items) {
+        setHeadline(withTitle: value.title)
+        setArticleType(value.typeName)
+        setUpdatedTime(value.shortTimestamp)
+    }
+    
+    private func setHeadline(withTitle title:String) {
+        articleTitle.text = title;
+    }
+    
+    private func setArticleType(_ type:String!) {
+        guard let articleType = type else {
+            articleTypeImage.isHidden = true;
+            return;
+        }
+        if(articleType == "Article") {
+            return;
+        }
+        articleTypeImage.isHidden = false;
+        let titleWithType:String = "    \(articleType)  |  \(articleTitle.text ?? "")"
+        articleTitle.text = titleWithType;
+    }
+    
+    private func setUpdatedTime(_ time:String) {
+        updatedTime.text = time;
+    }
+}
--- NBCHomeClone/ViewController/HomeScreenViewController.swift
+++ NBCHomeClone/ViewController/HomeScreenViewController.swift
@@ -18,13 +18,18 @@ class HomeScreenViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("HomeViewController loaded");
+        setDataSource();
         fetchData();
         dispatchGroup.notify(queue: .main, execute: { [weak self] in
             self?.loadTableView();
         })
     }
     
-    public func fetchData() {
+    private func setDataSource() {
+        newsTableView.dataSource = self
+    }
+    
+    private func fetchData() {
         dispatchGroup.enter();
         FetchDataFromAPI.getJsonArrayFromApi(fromUrl: HOME_API) {[weak self] (homeData:HomeDataModel) in
             self?.homeDataModel = homeData;
@@ -32,47 +37,45 @@ class HomeScreenViewController: UIViewController {
         }
     }
     
-    public func loadTableView() {
-        print("Reload Called")
+    private func loadTableView() {
+        print("Reload TableView Data")
         newsTableView.reloadData();
     }
 }
 
 //ListCell Population
-//extension HomeScreenViewController: UITableViewDataSource {
+extension HomeScreenViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let toReturn = homeDataModel?.modules[0].items.count ?? 0
+        return toReturn
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = newsTableView.dequeueReusableCell(withIdentifier: "listViewNewsCell") as! ListTableViewCell
+        cell.setValue(value: (homeDataModel?.modules[0].items[indexPath.row])!)
+        return cell;
+    }
+}
+//extension HomeScreenViewController:UITableViewDelegate,UITableViewDataSource{
 //    func numberOfSections(in tableView: UITableView) -> Int {
-//        return 1;
+//        1
 //    }
-//
 //    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        let toReturn = homeDataModel?.modules[0].items.count ?? 0
-//        print(toReturn)
-//        return toReturn
+//        5
 //    }
 //
 //    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        print("Getting Cell")
-//        let cell = newsTableView.dequeueReusableCell(withIdentifier: "listViewNewsCell") as! ListTableViewCell
-//        cell.setValue(value: (homeDataModel?.modules[0].items[indexPath.row])!)
-//        return cell;
+//
+//    let cell = tableView.dequeueReusableCell(withIdentifier: leadImageSectionIdentifier) as! LeadImageTableViewCell
+//        setGradientBackground(view:cell.gradientLayer)
+//        return cell
 //    }
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        150.0
+//    }
+//
 //}
-extension HomeScreenViewController:UITableViewDelegate,UITableViewDataSource{
-    func numberOfSections(in tableView: UITableView) -> Int {
-        1
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        5
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       
-    let cell = tableView.dequeueReusableCell(withIdentifier: leadImageSectionIdentifier) as! LeadImageTableViewCell
-        setGradientBackground(view:cell.gradientLayer)
-        return cell
-    }
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        150.0
-    }
-    
-}
