ebe1b4f2f6722ad1ab7ce396024d0440d5836fbe
LourdesDinesh <lourdesdinesh.s@tringapps.com>
2019-11-25 18:45:41 +0530

    Added the Setter method for all the TableViewCells that have been completed

---
+++ NBCHomeClone/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  NBCHomeClone
+//
+//  Created by Tringapps on 11/25/19.
+//  Copyright © 2019 Tringapps. All rights reserved.
+//
+
+import Foundation
---
+++ NBCHomeClone/TableViewCell/SectionNameTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SectionNameTableViewCell.swift
+//  NBCHomeClone
+//
+//  Created by Tringapps on 11/25/19.
+//  Copyright © 2019 Tringapps. All rights reserved.
+//
+
+import UIKit
+
+class SectionNameTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
